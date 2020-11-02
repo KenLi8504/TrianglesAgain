acea7f0869e513d54286cf1d24989ffbcd6cc76a
Kenli8504 <kli22@stuy.edu>
2020-11-01 20:25:26 -0500

    Created perimeter method

--- Point.java
+++ Point.java
@@ -27,3 +27,27 @@ public class Point{
     return (x == other.x && y == other.y);
   }
 }
+
+class Triangle{
+  private Point v1,v2,v3;
+
+  public Triangle(Point a, Point b, Point c){
+    v1 = a;
+    v2 = b;
+    v3 = c;
+  }
+
+  public Triangle (double x1, double y1, double x2, double y2, double x3, double y3){
+    v1 = new Point(x1,y1);
+    v2 = new Point(x2,y2);
+    v3 = new Point(x3,y3);
+  }
+
+  public double getPerimeter(){
+    double x,y,z;
+    x = v1.distanceTo(v2);
+    y = v2.distanceTo(v3);
+    z = v3.distanceTo(v1);
+    return (x+y+z);
+  }
+}
